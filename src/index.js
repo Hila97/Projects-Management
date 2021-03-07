0a216b16d8b4d02049127df715ac4a629518a57c
Hila Pinker <64475686+Hila97@users.noreply.github.com>
2021-03-07 23:48:04 +0200

    try
    1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+const app = express()
+
+app.get('/',(req,res)=>{
+ res.send("hello world")
+})
+app.get('/profile',(req,res)=>{
+ res.send("profile")
+})
+app.listen(port,()=>{
+ console.log(`server is up and running at: http://127.0.0.1:${port}`)
+})
