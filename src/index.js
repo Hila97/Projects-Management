0b7638e531139b86ad4310696145082f603d1408
Hila Pinker <64475686+Hila97@users.noreply.github.com>
2021-03-10 21:09:19 +0200

    try 2 after name change

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.get('/',(req,res)=>{
- res.send("hello world")
+ res.send('hello world')
 })
 app.get('/profile',(req,res)=>{
- res.send("profile")
+ res.send('profile')
 })
 app.listen(port,()=>{
  console.log(`server is up and running at: http://127.0.0.1:${port}`)
