37d858847a57d693773601ac2a4a61f3381979ab
mar426 <78439770+mar426@users.noreply.github.com>
2021-03-11 19:54:11 +0200

    hi

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.get('/',(req,res)=>{
- res.send('hello ')
+ res.send('hello world')
 })
 app.get('/profile',(req,res)=>{
  res.send('profile')
