37f1c751f671cea47dbf29b4500c2c34b3298571
hodaypi <62617707+hodaypi@users.noreply.github.com>
2021-03-11 17:33:05 +0200

    Hodaya

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 4000
 const app = express()
 
 app.get('/',(req,res)=>{
- res.send('Contractors management')
+ res.send('Contractors ')
 })
 app.get('/profile',(req,res)=>{
  res.send('profile')
