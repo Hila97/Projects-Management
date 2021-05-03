23b28ed95c18e26a0362b2b0742b36f5c00386bc
Hila Pinker <64475686+Hila97@users.noreply.github.com>
2021-05-04 00:08:33 +0300

    Update api.js

--- src/route/api.js
+++ src/route/api.js
@@ -2,8 +2,6 @@ const router=require('express').Router()
 const user = require('../controllers/userComtroller')
 const s=require('../controllers/SalaryController')
 const bankAccount = require('../controllers/BankAccountController')
-const bookingf = require('../controllers/bookingf')
-const bookingp = require('../controllers/bookingp')
 
 router.post('/addUser',(user.addUser))
 router.get('/findAllUsers',(user.findAllUsers))
@@ -11,8 +9,6 @@ router.get('/findUser/:username',user.findUser)
 router.post('/findUserById',(user.findUserById))
 router.post('/addBankAccount',(bankAccount.addBankAccount))
 router.post('/addSalary',s.addSalary)
-router.post('/bookingf',bookingf.booking)
-router.post('/bookingp',bookingp.booking)
 
 
 
