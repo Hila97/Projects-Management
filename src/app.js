16d480d6f159a3742464e4be8c237b27b8fa0f66
merav1 <80413780+merav1@users.noreply.github.com>
2021-05-11 12:20:36 +0300

    emp

--- src/app.js
+++ src/app.js
@@ -1,3 +1,110 @@
+// // //express
+// const express = require('express')
+// const app = express()
+// app.use(express.static('public'))
+// const axios = require('axios')
+// const moment = require('moment')
+
+// // //env
+// const dotenv=require('dotenv')
+// dotenv.config()
+
+// // //body parser
+// const bodyParser=require('body-parser')
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended:true}))
+
+// app.set('view engine','ejs')
+// const port = process.env.PORT || 3000
+
+// // //mongoose
+// const mongoose = require ('mongoose')
+// dbURI= 'mongodb+srv://Hodaya:hp1234@mhyhmcluster.d5gdr.mongodb.net/MHYHMdatabase?retryWrites=true&w=majority'
+// //connect to mongoDB
+// mongoose.connect(dbURI,{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+//     .then((result)=> {
+//      console.log('connected')
+//     })
+//     .catch ((err)=>console.log(err))
+
+// // //routers
+// app.use('/user',require('./route/api'))
+// app.use('/attendanceReport',require('./route/AttendanceReportAPI'))
+// app.use('/companyEmployee',require('./route/CompanyEmployeeAPI'))
+// app.use('/contractorWorker',require('./route/ContractorWorkerAPI'))
+// app.use('/employer',require('./route/EmployerAPI'))
+// app.use('/employment',require('./route/EmploymentAPI'))
+// app.use('/errorReport',require('./route/ErrorReportsAPI'))
+// app.use('/vacation',require('./route/VacationAPI'))
+// app.use('/api',require('./route/api'))
+// // ///try
+// app.use('/auth',require('./route/authAPI'))
+// //error handling
+// app.use(function (err,req,res,next){
+//     res.status(422).send({error:err.message})
+// })
+// app.listen(port,()=>{console.log(`server is up and running at: http://127.0.0.1:${port}`)})
+
+
+
+
+// // /*--------------------------------GET HTMLS---------------------------------*/
+// //loginController(app)
+// app.get('/', (req, res)=>{
+//  res.render('HomeNavUser')
+// })
+
+// app.get('/addContractorForm', (req, res)=>{
+//     res.render('addContractorForm')
+// })
+// app.get('/HomeNavUser', (req, res)=>{
+//     res.render('HomeNavUser')
+// })
+
+// app.get('/ContractorHome', (req, res)=>{
+//     res.render('HomeContractor')
+// })
+
+// app.get('/EmployerHome', (req, res)=>{
+//     res.render('HomeEmployer')
+// })
+
+// app.get('/EmployeeHome', (req, res)=>{
+//     res.render('HomeEmployee')
+// })
+
+// app.get('/login', (req, res)=>{
+//  res.render('login')
+// })
+
+// app.get('/attandenceReport', (req, res)=>{
+//     res.render('attandenceReport')
+// })
+// app.get('/profile/:name',(req, res) => {
+//  var data={age:23, job:'student'}
+//  res.render('profile',{person: req.params.name,data:data})
+// })
+// app.get('/SearchContractorWorker',((req, res) => {
+//     res.render('SearchContractorWorker')
+// }))
+
+// app.get('/getBookedEmployeesToday',(req, res) => {
+//     try {
+//         const fetchBooking = async () =>{
+//            const {data} = await axios.get('http://127.0.0.1:3000/employer/getBookedEmployeesToday') 
+//            if(typeof data === 'string'){
+//                 return res.render('Error', {message : data})
+//            }
+//            else{
+//             const workerArr = data.map( d => d.workerID)
+//             res.render('getBookedEmployeesToday',{workerArr : workerArr})
+//            }
+//         }
+//         fetchBooking()
+//     } catch (e) {
+//         console.log(e)
+//     }
+
 //express
 const express = require('express')
 const app = express()
